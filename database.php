22eca3c859aa01210c3b3db86047d020fbd72396
Md. Golam Rabby <32569556+Rabby312@users.noreply.github.com>
2020-10-23 22:21:11 +0600

    Update database.php

--- database.php
+++ database.php
@@ -1,4 +1,5 @@
 <?php
+//database connection for online exam
 $con=mysqli_connect("localhost","root","","quiz") or die("Could not Connect My Sql");
 //mysqli_select_db("quiz",$cn)  or die("Could connect to Database");
 ?>
