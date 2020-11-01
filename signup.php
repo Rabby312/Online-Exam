17ab1d9bef23da8f970b8552267a9bf51732a730
Md. Golam Rabby <32569556+Rabby312@users.noreply.github.com>
2020-11-01 13:08:42 +0600

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,5 +1,6 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+<!-- sign up for user -->
 <head>
 <title>New user signup </title>
 <script language="javascript">
