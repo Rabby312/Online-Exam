43370d2e4a691a790412ab9bf573f84244dc4abe
Md. Golam Rabby <32569556+Rabby312@users.noreply.github.com>
2020-11-02 13:20:44 +0600

    Update result.php

--- result.php
+++ result.php
@@ -3,6 +3,7 @@ session_start();
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+<!--this is result page for online quiz-->
 <head>
 <title>Online Quiz  - Result </title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
